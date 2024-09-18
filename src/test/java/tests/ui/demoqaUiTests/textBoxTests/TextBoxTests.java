3d4620184e1252a8cff33bc3a220d6c0c5b5e0a7
anisimovpk <anisimovpk@yoomoney.ru>
2024-09-18 14:37:22 +0300

    1) Новая структура проекта 2) добавил отчеты Allure

--- src/test/java/tests/ui/demoqaUiTests/textBoxTests/TextBoxTests.java
+++ src/test/java/tests/ui/demoqaUiTests/textBoxTests/TextBoxTests.java
@@ -1,6 +1,8 @@
 package tests.ui.demoqaUiTests.textBoxTests;
 
 import dataqa.mainPage.page_object.pages.BasePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tests.ui.demoqaUiTests.BaseTest;
@@ -8,10 +10,13 @@ import tests.ui.demoqaUiTests.steps.TextBoxSteps;
 
 public class TextBoxTests extends BaseTest {
     TextBoxSteps textBoxSteps = new TextBoxSteps();
+
     BasePage basePage = new BasePage();
 
     @Test
+    @Owner("Имя тестировщика")
     @DisplayName("Проверяем заполененные поля на странице  /text-box")
+    @Description("Тест заполняет все поля, нажимает кнопку submit и проверяет вывод заполненной информации")
     void fillAllForms() {
         String fullName = "Name LastName";
         String email = "email@email.ru";
@@ -27,5 +32,4 @@ public class TextBoxTests extends BaseTest {
         textBoxSteps.clickBtn();
         textBoxSteps.completedFormShouldHaveInfo(fullName, email, currentAddress, permanentAddress);
     }
-
 }
